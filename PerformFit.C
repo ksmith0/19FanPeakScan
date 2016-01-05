b74f4cf9241af8838148daefd6e26d1d7abe7cae
William Peters <wapeters.delien@gmail.com>
2016-01-05 16:24:17 -0500

    Unc calc correction and matrix multiplication.

--- PerformFit.C
+++ PerformFit.C
@@ -1,12 +1,16 @@
 //Author: Karl Smith
+//Editted by WAP
 
 #include <fstream>
 #include <sstream>
 #include <string>
+// #include <vector>
 #include "TF1.h"
 #include "TFitResult.h"
 #include "TGraphErrors.h"
-#include "TStyle.h"
+#include "TStyle.h" 
+#include "TMatrixD.h"
+// #include "MatrixFunctions.h"
 
 #if defined(__CINT__) && !defined(__MAKECINT__) 
 	#include "PeakFit.so"
@@ -31,18 +35,21 @@ TGraphErrors *PerformFit(const char *expFilename, const char *simFilename) {
 		//printf("Found bar %d\n",barNum);
 
 		float x0 = -1;
-		double A = 0, dA = 100;
-		double sumA = 0, dSumA = 0;
+		double A = 0.0, dA = 100.0;
+		double sumA = 0.0, dSumA = 0.0, dSumAcalc = 0.0;
 		bool first = true;
 		while (linestr >> x0 >> A >> dA) {
 			if (first) {
 				first = false;
 				continue;
 			}
+			//maybe set all dA to 300 or if too large set to sqrt(A)
+			if (dA < 50) {dA = 50 + sqrt(A);}
+			if (dA > 500) {dA = 300 + sqrt(A);}
 			sumA += A;
-			dSumA += sqrt(pow(dSumA,2) + pow(dA,2));
+			dSumAcalc = dSumA;
+			dSumA = sqrt(pow(dSumAcalc,2) + pow(dA,2));
 		}
-
 		//Insert the point into the graph.
 		int point = graph->GetN();
 		graph->SetPoint(point, barNum, sumA);
@@ -73,6 +80,35 @@ TGraphErrors *PerformFit(const char *expFilename, const char *simFilename) {
 	//For example print the Chi Sq
 	printf("The Chi2 is: %f / %u\n", fitResult->Chi2(), fitResult->Ndf());
 
+
+//define matrix and vector variables
+
+	int nparStates = fitResult->NPar();
+	double fitvalues[nparStates];
+	double totalerror;
+	TMatrixDSym Covar(nparStates);
+	TMatrixDSym Corr(nparStates);
+
+//set matrix and vectors from fit
+
+		for (int i=0;i<nparStates;i++) {
+			fitvalues[i] = f->GetParameter(i);
+		}
+
+
+	Covar = fitResult->GetCovarianceMatrix();
+	Corr = fitResult->GetCorrelationMatrix();
+/*
+//Calculate total error from Covar matrix and fitvalues vector
+	totalerror = fitvalues * Covar * fitvalues;
+
+	//GetCovarMatrix
+	//GetVarianceMatrix
+	//Print them
+	//do math with vector of results GetComponents
+	//Output result of fit plus uncertainty
+
+*/
 	printf("The Covariance Matrix:\n");
 	printf("Status: ");
 	switch(fitResult->CovMatrixStatus()) {
@@ -98,7 +134,12 @@ TGraphErrors *PerformFit(const char *expFilename, const char *simFilename) {
 		}
 		printf("\n");
 	}
-
+	for (unsigned int i=0;i<nparStates;i++) {
+		for (unsigned int j=0;j<nparStates;j++) {
+			printf("%8e, ",Covar(i,j));
+		}
+		printf("\n");
+	}
 	printf("The Correlation Matrix:\n");
 	for (unsigned int i=0;i<fitResult->NPar();i++) {
 		for (unsigned int j=0;j<fitResult->NPar();j++) {
